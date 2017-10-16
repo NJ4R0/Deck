4af47249e3ce2cc9eb9bb0765bc063c3060424fb
NJ4R0 <32643481+NJ4R0@users.noreply.github.com>
2017-10-16 10:18:04 +0200

    added bugged test
    added bugged test

--- test/DeckTestJ4.java
+++ test/DeckTestJ4.java
@@ -27,4 +27,13 @@ public class DeckTestJ4 extends TestCase {
         assertTrue(deck.getFirst().compareTo(new Card(Color.CLUBS, Value.TWO))==0);
     }
 
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testException() throws Exception{
+        try{
+            Card Joker = deck.getCards().get(53);
+        } catch (IndexOutOfBoundsException ex){
+            throw ex;
+        }
+    }
+
 }
